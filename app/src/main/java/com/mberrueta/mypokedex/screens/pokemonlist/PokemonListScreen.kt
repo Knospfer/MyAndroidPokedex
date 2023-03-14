61c918d767463b22b77e4ea31ba20c19149d4f7f
Mattia Berrueta <berrueta.mattia@gmail.com>
2023-03-14 17:47:16 +0100

    feat: pokemon card list

--- app/src/main/java/com/mberrueta/mypokedex/screens/pokemonlist/PokemonListScreen.kt
+++ app/src/main/java/com/mberrueta/mypokedex/screens/pokemonlist/PokemonListScreen.kt
@@ -1,20 +1,27 @@
 package com.mberrueta.mypokedex.screens.pokemonlist
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material.Scaffold
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.SubcomposeAsyncImage
+import com.mberrueta.mypokedex.core.entities.PokemonEntry
 
 @Composable
 fun PokemonListScreen() {
@@ -32,21 +39,89 @@ fun PokemonListScreen() {
 fun PageContent(viewModel: PokemonListScreenViewModel = hiltViewModel()) {
     val pokemonList by remember { viewModel.completePokemonList }
 
-    Column(modifier = Modifier.padding(horizontal = 16.dp)) {
-        Text(text = "Pokedex", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+    Column {
+        Text(
+            text = "Pokedex",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
         Spacer(modifier = Modifier.height(24.dp))
-        LazyVerticalGrid(columns = GridCells.Fixed(2), content = {
-            items(pokemonList.size) { index ->
-                Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier
-                        .height(100.dp)
-                        .fillMaxWidth(),
-                ) {
-                    Text(text = pokemonList[index].name)
+        LazyVerticalGrid(
+            modifier = Modifier.padding(horizontal = 8.dp),
+            columns = GridCells.Fixed(2),
+            content = {
+                items(pokemonList.size) { index ->
+                    PokemonCard(pokemonList[index])
                 }
+            })
+    }
+}
 
+@Composable
+fun PokemonCard(
+    pokemonEntry: PokemonEntry,
+    viewModel: PokemonListScreenViewModel = hiltViewModel()
+) {
+    val defaultDominantColor = MaterialTheme.colors.surface
+    var dominantColor by remember {
+        mutableStateOf(defaultDominantColor)
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .shadow(4.dp, RoundedCornerShape(10.dp))
+            .aspectRatio(1f)
+            .background(dominantColor)
+            .clickable {
+                //todo: naviga a dettaglio
             }
-        })
+    ) {
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            SubcomposeAsyncImage(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .align(CenterHorizontally),
+                model = pokemonEntry.urlImage,
+                contentDescription = pokemonEntry.name,
+                loading = {
+                    CircularProgressIndicator(
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier.scale(0.5f)
+                    )
+                },
+                onSuccess = {
+                    viewModel.getDominantColor(it.result.drawable) { color ->
+                        dominantColor = color
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                verticalAlignment = Alignment.Bottom
+            ) {
+                Text(
+                    text = "#${pokemonEntry.number}",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = pokemonEntry.name,
+                    fontSize = 18.sp,
+                    modifier = Modifier.fillMaxWidth(),
+                    maxLines = 1,
+                    overflow = TextOverflow.Ellipsis
+                )
+            }
+        }
     }
 }
--- app/src/main/java/com/mberrueta/mypokedex/screens/pokemonlist/PokemonListScreenViewModel.kt
+++ app/src/main/java/com/mberrueta/mypokedex/screens/pokemonlist/PokemonListScreenViewModel.kt
@@ -1,8 +1,13 @@
 package com.mberrueta.mypokedex.screens.pokemonlist
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.palette.graphics.Palette
 import com.mberrueta.mypokedex.core.definitions.Resource
 import com.mberrueta.mypokedex.core.entities.PokemonEntry
 import com.mberrueta.mypokedex.core.repository.PokemonRepository
@@ -26,6 +31,16 @@ class PokemonListScreenViewModel @Inject constructor(
 
             if (response is Resource.Success<List<PokemonEntry>>) {
                 completePokemonList.value = response.data!!
+            }
+        }
+    }
+
+    fun getDominantColor(drawable: Drawable, onFinish: (Color) -> Unit) {
+        val bmp = (drawable as BitmapDrawable).bitmap.copy(Bitmap.Config.ARGB_8888, true)
+
+        Palette.from(bmp).generate { palette ->
+            palette?.dominantSwatch?.rgb?.let { colorValue ->
+                onFinish(Color(colorValue))
             }
         }
     }
