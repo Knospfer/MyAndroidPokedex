ab74d7c4a84980d0c2dd387b293823b07c9991a7
Mattia Berrueta <berrueta.mattia@gmail.com>
2023-03-14 16:26:19 +0100

    feat: pokemon list screen blank page

--- app/src/main/java/com/mberrueta/mypokedex/MainActivity.kt
+++ app/src/main/java/com/mberrueta/mypokedex/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.mberrueta.mypokedex
 
-import android.app.Application
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,36 +10,27 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.mberrueta.mypokedex.core.theme.MyPokedexTheme
-import dagger.hilt.android.HiltAndroidApp
+import com.mberrueta.mypokedex.screens.pokemonlist.PokemonListScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyPokedexTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "pokemon_list_screen") {
+                    composable("pokemon_list_screen") {
+                        PokemonListScreen()
+                    }
+                    //TODO: pokemon detail screen
                 }
             }
         }
     }
 }
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyPokedexTheme {
-        Greeting("Android")
-    }
-}
-
---
+++ app/src/main/java/com/mberrueta/mypokedex/screens/pokemonlist/PokemonListScreen.kt
@@ -0,0 +1,12 @@
+package com.mberrueta.mypokedex.screens.pokemonlist
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun PokemonListScreen() {
+    Box() {
+        Text(text = "Hi")
+    }
+}
