11e3d447cdda40b74349802d01f159c5f2f29cdc
Mattia Berrueta <berrueta.mattia@gmail.com>
2023-03-15 15:12:28 +0100

    feat: statistics

--- app/src/main/java/com/mberrueta/mypokedex/screens/pokemondetail/PokemonDetailScreen.kt
+++ app/src/main/java/com/mberrueta/mypokedex/screens/pokemondetail/PokemonDetailScreen.kt
@@ -2,11 +2,14 @@ package com.mberrueta.mypokedex.screens.pokemondetail
 
 import android.annotation.SuppressLint
 import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
@@ -17,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -25,6 +29,8 @@ import coil.compose.AsyncImage
 import com.mberrueta.mypokedex.core.definitions.Resource
 import com.mberrueta.mypokedex.core.models.Pokemon
 import com.mberrueta.mypokedex.core.models.Stat
+import com.mberrueta.mypokedex.core.utils.parseStatToAbbr
+import com.mberrueta.mypokedex.core.utils.parseStatToColor
 import com.mberrueta.mypokedex.core.utils.parseTypeToColor
 import java.util.*
 
@@ -93,7 +99,7 @@ fun DetailView(pokemon: Pokemon) {
             contentDescription = pokemon.name,
             modifier = Modifier.size(200.dp)
         )
-        //TODO STATISTICS
+        PokemonBaseStats(pokemonInfo = pokemon)
     }
 }
 
@@ -116,6 +122,88 @@ fun TypeSection(pokemon: Pokemon, modifier: Modifier = Modifier) {
                 }
             }
             Spacer(modifier = Modifier.width(8.dp))
+        }
+    }
+}
+
+@Composable
+fun PokemonBaseStats(
+    pokemonInfo: Pokemon,
+    animationDelay: Int = 100,
+) {
+    val maxBaseState = remember {
+        pokemonInfo.stats.maxOf { it.base_stat }
+    }
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Text(text = "Base stats:", fontSize = 20.sp, color = MaterialTheme.colors.onSurface)
+        Spacer(modifier = Modifier.height(4.dp))
+
+        for (i in pokemonInfo.stats.indices) {
+            val stat = pokemonInfo.stats[i]
+
+            PokemonStat(
+                statName = parseStatToAbbr(stat),
+                statValue = stat.base_stat,
+                statMaxValue = maxBaseState,
+                statColor = parseStatToColor(stat),
+                animationDelay = i * animationDelay,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+        }
+    }
+}
+
+@Composable
+fun PokemonStat(
+    statName: String,
+    statValue: Int,
+    statMaxValue: Int,
+    statColor: Color,
+    height: Dp = 28.dp,
+    animationDuration: Int = 1000,
+    animationDelay: Int = 0,
+) {
+    var animationPLayed by remember {
+        mutableStateOf(false)
+    }
+    val currentPercent =
+        animateFloatAsState(
+            targetValue = if (animationPLayed) {
+                statValue / statMaxValue.toFloat()
+            } else 0f,
+            animationSpec = tween(
+                animationDuration,
+                animationDelay
+            )
+        )
+    LaunchedEffect(key1 = true) { animationPLayed = true }
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(height)
+            .clip(CircleShape)
+            .background(
+                if (isSystemInDarkTheme()) {
+                    Color(0xff505050)
+                } else Color.LightGray
+            )
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(currentPercent.value) //è questa la cosa che si animerà
+                .background(statColor)
+                .padding(horizontal = 8.dp)
+        ) {
+            Text(text = statName, fontWeight = FontWeight.Bold)
+            Text(
+                text = (currentPercent.value * statMaxValue).toInt().toString(),
+                fontWeight = FontWeight.Bold
+            )
         }
     }
 }
